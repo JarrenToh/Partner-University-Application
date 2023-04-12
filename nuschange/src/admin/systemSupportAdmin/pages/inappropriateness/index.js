46d49401eb69d6ad01a0fdc808077e6ee21c8bac
wjahoward <howardwang42@gmail.com>
2023-04-12 20:48:36 +0800

    inappropriate content

--- nuschange/src/admin/systemSupportAdmin/pages/inappropriateness/index.js
+++ nuschange/src/admin/systemSupportAdmin/pages/inappropriateness/index.js
@@ -8,76 +8,90 @@ import InappropriatenessComponent from "./components";
 import apiPaths from "../../../../util/apiPaths";
 
 const Inappropriateness = () => {
+  const [selectedButton, setSelectedButton] = useState("PUReview");
 
-    const [selectedButton, setSelectedButton] = useState("PUReview");
+  const handleButtonClicks = (category) => {
+    setSelectedButton(category);
+  };
 
-    const handleButtonClicks = (category) => {
-        setSelectedButton(category);
-    }
+  const buttons = [
+    { label: "PUReview", category: "PUReview" },
+    { label: "ForumComment", category: "ForumComment" },
+    { label: "ForumPost", category: "ForumPost" },
+    { label: "ForumTopic", category: "ForumTopic" },
+    { label: "PUModuleReview", category: "PUModuleReview" },
+  ];
 
-    const buttons = [{ label: "PUReview", category: "PUReview" }, { label: "ForumComment", category: "ForumComment" }, { label: "ForumPost", category: "ForumPost" }, { label: "ForumTopic", category: "ForumTopic" }, { label: "PUModuleReview", category: "PUModuleReview" },];
+  const additionalProps = {};
 
-    const additionalProps = {};
+  switch (selectedButton) {
+    case "PUReview":
+      additionalProps.type = "Review";
+      additionalProps.typeOfComponent = "PUReview";
+      additionalProps.apiPath = `${apiPaths.listOfPUReviews}/getAllReportedPUReviews`;
+      break;
+    case "ForumComment":
+      additionalProps.type = "Message";
+      additionalProps.typeOfComponent = "ForumComment";
+      additionalProps.apiPath = `${apiPaths.listOfForumComments}`;
+      break;
+    case "ForumPost":
+      additionalProps.type = "Message";
+      additionalProps.typeOfComponent = "ForumPost";
+      additionalProps.apiPath = `${apiPaths.listOfForumPosts}`;
+      break;
+    case "ForumTopic":
+      additionalProps.type = "Topic Name";
+      additionalProps.typeOfComponent = "ForumTopic";
+      additionalProps.apiPath = `${apiPaths.listOfForumTopics}`;
+      break;
+    case "PUModuleReview":
+      additionalProps.type = "Review";
+      additionalProps.typeOfComponent = "PUModuleReview";
+      additionalProps.apiPath = `${apiPaths.listOfPUModuleReview}`;
+      break;
+    default:
+      additionalProps.type = "Review";
+      additionalProps.typeOfComponent = "PUReview";
+      additionalProps.apiPath = `${apiPaths.listOfPUReviews}/getAllReportedPUReviews`;
+  }
 
-    switch (selectedButton) {
-        case "PUReview":
-            additionalProps.type = "Review";
-            additionalProps.typeOfComponent = "PUReview";
-            additionalProps.apiPath = `${apiPaths.listOfPUReviews}/getAllReportedPUReviews`;
-            break;
-        case "ForumComment":
-            additionalProps.type = "Message";
-            additionalProps.typeOfComponent = "ForumComment";
-            additionalProps.apiPath = `${apiPaths.listOfForumComments}`;
-            break;
-        case "ForumPost":
-            additionalProps.type = "Message";
-            additionalProps.typeOfComponent = "ForumPost";
-            additionalProps.apiPath = `${apiPaths.listOfForumPosts}`;
-            break;
-        case "ForumTopic":
-            additionalProps.type = "Topic Name";
-            additionalProps.typeOfComponent = "ForumTopic";
-            additionalProps.apiPath = `${apiPaths.listOfForumTopics}`;
-            break;
-        case "PUModuleReview":
-            additionalProps.type = "Review";
-            additionalProps.typeOfComponent = "PUModuleReview";
-            additionalProps.apiPath = `${apiPaths.listOfPUModuleReview}`;
-            break;
-        default:
-            additionalProps.type = "Review";
-            additionalProps.typeOfComponent = "PUReview";
-            additionalProps.apiPath = `${apiPaths.listOfPUReviews}/getAllReportedPUReviews`;
-    }
-
-    return (
-        <div>
-            <Header />
-            <Menu />
-            <div className="content-wrapper">
-                <div className="card">
-                    <div className="card-header">
-                        <h3 className="card-title">Inappropriateness Content</h3>
-                        <div className="btn-group">
-                            {buttons.map(({ label, category }) => (
-                                <button
-                                    key={category}
-                                    type="button"
-                                    className={`btn ${category === selectedButton ? "btn-primary" : "btn-outline-dark"}`}
-                                    onClick={() => handleButtonClicks(category)}
-                                >
-                                    {label}
-                                </button>
-                            ))}
-                        </div>
-                    </div>
-                    <InappropriatenessComponent {...additionalProps} selectedButton={selectedButton} />
-                </div>
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <div className="content-wrapper">
+        <div className="card">
+          <div className="card-header d-flex justify-content-between align-items-center">
+            <h3 className="card-title">Inappropriateness Content</h3>
+            <div className="btn-group">
+              {buttons.map(({ label, category }) => (
+                <button
+                  key={category}
+                  type="button"
+                  className={`btn ${
+                    category === selectedButton
+                      ? "btn-primary"
+                      : "btn-outline-dark"
+                  }`}
+                  onClick={() => handleButtonClicks(category)}
+                >
+                  {label}
+                </button>
+              ))}
             </div>
-            <Footer />
+          </div>
+          <div className="card-body">
+            <InappropriatenessComponent
+              {...additionalProps}
+              selectedButton={selectedButton}
+            />
+          </div>
         </div>
-    )
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Inappropriateness;
